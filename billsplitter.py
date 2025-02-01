75005e82ca0cad4eaf3bc5e0dff027d84137c141
Margam Namratha <namrathamargam@gmail.com>
2025-02-01 23:10:14 +0530

    first commit

--- billsplitter.py
+++
@@ -1,9 +0,0 @@
-no_of_people=int(input("enter no of people:"))
-total_bill=int(input("enter total bill:"))
-tip=int(input("tip you wants to provide:"))
-tip_amount=total_bill*(tip/100)
-final=total_bill+tip_amount
-each_person=final/no_of_people
-print("Then each person have to pay:",each_person)
-print(f"Tip Amount: {tip_amount}")
-print(f"Final amount paid with tip: {final}")
